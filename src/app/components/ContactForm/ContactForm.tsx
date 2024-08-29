7a3929a1f013c68ee97f3ebea184e523679e40cb
juliansirkin <juliansirkin@gmail.com>
2024-08-29 16:06:22 -0700

    clean up form after success or failure

--- src/app/components/ContactForm/ContactForm.tsx
+++ src/app/components/ContactForm/ContactForm.tsx
@@ -2,11 +2,16 @@
 
 import { useState, FormEvent } from "react";
 
+const successToastMessage = "Comment Successfully emailed";
+const failToastMessage =
+  "Failed to pass along message, please email or dm directly";
+
 export const ContactForm = () => {
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   const [comment, setComment] = useState("");
+  const [formSubmissionMessage, setFormSubmissionMessage] = useState("");
 
   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,11 +24,26 @@ export const ContactForm = () => {
       method: "POST",
       body: JSON.stringify({ name, phoneNumber, email, comment }),
     });
-    console.log(await response.json());
+    if (response.status === 200) {
+      setFormSubmissionMessage(successToastMessage);
+
+      setComment("");
+    } else {
+      setFormSubmissionMessage(failToastMessage);
+    }
+    setName("");
+    setPhoneNumber("");
+    setEmail("");
+    setTimeout(() => {
+      setFormSubmissionMessage("");
+    }, 10000);
   };
 
   return (
-    <form className="flex flex-col gap-4 w-96" onSubmit={handleFormSubmit}>
+    <form
+      className="flex flex-col gap-4 mx-auto w-11/12 md:w-96 justify-center"
+      onSubmit={handleFormSubmit}
+    >
       <span className="flex flex-col">
         <label>Name</label>
         <input
@@ -60,6 +80,7 @@ export const ContactForm = () => {
       <span className="flex flex-col">
         <label>Comment</label>
         <textarea
+          className="h-32"
           id="comment"
           name="comment"
           placeholder="comment"
@@ -73,6 +94,22 @@ export const ContactForm = () => {
       >
         Submit Inquiry
       </button>
+      <div
+        className={`h-auto p-2 text-center text-xl font-bold ${
+          formSubmissionMessage === successToastMessage
+            ? "text-green-700 bg-black"
+            : ""
+        }
+        ${
+          formSubmissionMessage === failToastMessage
+            ? "text-red-600 bg-black"
+            : ""
+        }
+        `}
+        aria-label="Contact form submission toast"
+      >
+        {formSubmissionMessage}
+      </div>
     </form>
   );
 };
