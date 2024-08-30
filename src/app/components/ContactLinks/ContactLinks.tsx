a3b8dc9c9ca0c9ac4d70c6bec6081fffaa0dda85
juliansirkin <juliansirkin@gmail.com>
2024-08-29 22:39:46 -0700

    Create base links for contact module

---
+++ src/app/components/ContactLinks/ContactLinks.tsx
@@ -0,0 +1,41 @@
+import { FaInstagram } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { AiFillTikTok } from "react-icons/ai";
+import Link from "next/link";
+
+export const ContactLinks = () => {
+  return (
+    <ul className="flex flex-col gap-4">
+      <li className="flex gap-4 text-3xl font-bold">
+        <MdEmail />
+        <Link href="mailto:sirkinsupperclub@gmail.com">
+          sirkinsupperclub@gmail.com
+        </Link>
+      </li>
+      <li className="text-3xl font-bold">
+        <Link
+          href="https://www.instagram.com/julian.sirkin/"
+          className="flex gap-4"
+        >
+          <FaInstagram /> Julian.Sirkin
+        </Link>
+      </li>
+      <li className=" text-3xl font-bold">
+        <Link
+          href="https://www.instagram.com/sirkinsupperclub/"
+          className="flex gap-4"
+        >
+          <FaInstagram /> sirkinsupperclub
+        </Link>
+      </li>
+      <li className="flex gap-4 text-3xl font-bold">
+        <Link
+          href="https://www.tiktok.com/@sirkinsupperclub"
+          className="flex gap-4"
+        >
+          <AiFillTikTok /> sirkinsupperclub
+        </Link>
+      </li>
+    </ul>
+  );
+};
--- src/app/components/Modules/ContactModule/ContactModule.tsx
+++ src/app/components/Modules/ContactModule/ContactModule.tsx
@@ -1,10 +1,14 @@
 import { ContactForm } from "../../ContactForm/ContactForm";
+import { ContactLinks } from "../../ContactLinks/ContactLinks";
 
 const ContactModule = () => {
   return (
     <section id="contact" className="bg-gold h-auto p-8">
       <h2 className="text-xl md:text-5xl text-white text-center">Contact</h2>
-      <ContactForm />
+      <div className="flex flex-col md:flex-row justify-center gap-12">
+        <ContactForm />
+        <ContactLinks />
+      </div>
     </section>
   );
 };
