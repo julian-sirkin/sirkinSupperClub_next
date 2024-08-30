10df06b120fd0225710130d84ed898ef718e65e3
juliansirkin <juliansirkin@gmail.com>
2024-08-30 10:50:57 -0700

    add heading margin-bottom

--- src/app/components/Modules/ContactModule/ContactModule.tsx
+++ src/app/components/Modules/ContactModule/ContactModule.tsx
@@ -4,7 +4,7 @@ import { ContactLinks } from "../../ContactLinks/ContactLinks";
 const ContactModule = () => {
   return (
     <section id="contact" className="bg-gold h-auto p-8">
-      <h2 className="text-2xl md:text-5xl text-white text-center font-bold">
+      <h2 className="text-2xl md:text-5xl text-white text-center font-bold mb-4">
         Contact
       </h2>
       <div className="flex flex-col-reverse md:flex-row justify-center gap-4 md:gap-12">
