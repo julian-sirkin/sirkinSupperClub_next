516a1b8cdf8e39b1e2f37795926e4625399e1577
juliansirkin <juliansirkin@gmail.com>
2024-09-03 19:35:53 -0700

    change join to left join to only get back the ticket

--- src/app/api/queries/select.ts
+++ src/app/api/queries/select.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { customersTable, eventsTable, SelectCustomer, ticketsTable } from "@/db/schema";
 import { eq, and} from "drizzle-orm";
-import { GetTicketByIdAndEventProps, PurchasedTickets } from "../api.types";
+import { DatabaseTickets, GetTicketByIdAndEventProps, PurchasedTickets } from "../api.types";
 
 export async function getCustomerByEmail(email: SelectCustomer['email']){
 return db.select().from(customersTable).where(eq(customersTable.email, email));
@@ -11,16 +11,15 @@ return db.select().from(customersTable).where(eq(customersTable.email, email));
 
 export async function getTicketsByIdAndEvent(
     ticketEventProps: GetTicketByIdAndEventProps[]
-) {
+): Promise <DatabaseTickets[]> {
     const tickets = await Promise.all(
         ticketEventProps.map(({ ticketContentfulId, eventContentfulId }) =>
             db
                 .select({
                     ticket: ticketsTable,
-                    event: eventsTable,
                 })
                 .from(ticketsTable)
-                .innerJoin(eventsTable, eq(ticketsTable.event, eventsTable.id))
+                .leftJoin(eventsTable, eq(ticketsTable.event, eventsTable.id))
                 .where(
                     and(
                         eq(ticketsTable.contentfulId, ticketContentfulId),
