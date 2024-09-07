821dd11405cb2f493e57f112e390fafdd8844bca
juliansirkin <juliansirkin@gmail.com>
2024-09-06 22:32:34 -0700

    remove fixture data accidentally added to wrong file, but didn't save despite being delted

--- __test__/helpers/sortEventsByTime/sortEventsByTime.fixture.ts
+++ __test__/helpers/sortEventsByTime/sortEventsByTime.fixture.ts
@@ -34,46 +34,4 @@ export const eventsFixture: ParsedEvent[] = [
     }
   ]
 
-  const mockCartTicket1: CartTicketType = {
-    contentfulTicketId: 'ticket1',
-    eventContentfulId: 'event1',
-    quantity: 2,
-    price: 100,
-    time: new Date(),
-    title: 'Test',
-    isAddonTicket: false,
-    ticketsAvailable: 10
-};
-
-const mockCartTicket2: CartTicketType = {
-    contentfulTicketId: 'ticket2',
-    eventContentfulId: 'event2',
-    quantity: 3,
-    price: 100,
-    time: new Date(),
-    title: 'Test',
-    isAddonTicket: false,
-    ticketsAvailable: 10
-};
-
-const mockDatabaseTicket1: DatabaseTickets = {
-    ticket: {
-        time: null,
-        id: 1,
-        event: 1,
-        contentfulId: 'ticket1',
-        totalAvailable: 10,
-        totalSold: 5
-    }
-};
-
-const mockDatabaseTicket2: DatabaseTickets = {
-    ticket: {
-        time: null,
-        id: 2,
-        event: 2,
-        contentfulId: 'ticket2',
-        totalAvailable: 10,
-        totalSold: 3
-    }
-};
+  
